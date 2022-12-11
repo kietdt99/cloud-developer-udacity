408269fc712f4c6b018a2aca844dd2293a9b2877
Do Tuan Kiet <tuankiet141199@gmail.com>
2022-12-11 13:36:15 +0700

    Project 03 - Refactor Monolith to Microservices and Deploy
    upload final source code

--- Project_03/udagram-frontend/src/environments/environment.prod.ts
+++ Project_03/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: 'Udagram Microservices App',
+  apiHost: 'http://a3b19b20464e64b17b3fb62c5f43fba9-521351809.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
